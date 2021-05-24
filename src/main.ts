72566d07f9798e050053d0a77a3cc63c72f2b713
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2021-05-24 17:48:10 -0300

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   console.log(`Server listening on ${PORT}`);
   await app.listen(PORT, '0.0.0.0');
 }
